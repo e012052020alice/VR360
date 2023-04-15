c981bb90413fa108243a826611a95449ea1dd169
e012052020alice <119144400+e012052020alice@users.noreply.github.com>
2023-04-15 15:52:57 +0800

    Update 跳場景

---
+++ Assets/Scripts/ChangePosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+	public GameObject Player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.transform.position=new Vector3(0,5,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
